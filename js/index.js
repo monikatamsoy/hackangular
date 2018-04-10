8f214c09d97b6f812130b94d77bc3e07889571a5
monikatamsoy <tamsoymonika@gmail.com>
2018-04-10 23:11:02 +0530

    add comments

--- js/index.js
+++ js/index.js
@@ -3,11 +3,22 @@ var app = angular.module('app1', [])
 
 
 app.controller('storiesController', function($scope, $http) {
-  $http.get('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty').then(function(data) {
+
+  $scope.showComments = function() {
+    $scope.comments = []
+    if (this.m.descendants) {
+      var top20 = this.m.kids.slice(0, 19)
+      top20.map(function(comment) {
+        $http.get('https://hacker-news.firebaseio.com/v0/item/' + comment + '.json?print=pretty').then(function(data) {
+          $scope.comments.push(data.data)
+        })
+      })
+    }
+  }
+  $http.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty').then(function(data) {
     var storyIDs = data.data.slice(0, 19)
     $scope.stories = []
     for (var i = 0; i < storyIDs.length; i++) {
-      console.log(i)
       $http.get('https://hacker-news.firebaseio.com/v0/item/'+ storyIDs[i] +'.json?print=pretty').then(function(data) {
         $scope.stories.push(data.data)
       })
@@ -20,4 +31,17 @@ app.controller('storiesController', function($scope, $http) {
 
 
   })
+
+})
+
+app.filter('unsafe', function($sce) {
+  return $sce.trustAsHtml
+})
+
+app.filter('timeAgo', function($sce) {
+  return function(x) {
+    var posted = new Date(x)
+    posted = posted.toUTCString()
+    return posted
+  }
 })
