d9dc2fd517a2b7f2a017522e61d6badfede995f8
monikatamsoy <tamsoymonika@gmail.com>
2018-04-10 23:45:14 +0530

    add load more

--- js/index.js
+++ js/index.js
@@ -5,18 +5,22 @@ var app = angular.module('app1', [])
 app.controller('storiesController', function($scope, $http) {
 
   $scope.showComments = function() {
+    $scope.loadingcomments = 1
     $scope.comments = []
     if (this.m.descendants) {
       var top20 = this.m.kids.slice(0, 19)
       top20.map(function(comment) {
         $http.get('https://hacker-news.firebaseio.com/v0/item/' + comment + '.json?print=pretty').then(function(data) {
           $scope.comments.push(data.data)
+          $scope.loadingcomments = 0
         })
       })
     }
   }
   $http.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty').then(function(data) {
-    var storyIDs = data.data.slice(0, 19)
+    var storyIDs = data.data.slice(0, 20)
+    $scope.allStories = data.data
+    $scope.fetched = 20
     $scope.stories = []
     for (var i = 0; i < storyIDs.length; i++) {
       $http.get('https://hacker-news.firebaseio.com/v0/item/'+ storyIDs[i] +'.json?print=pretty').then(function(data) {
@@ -31,6 +35,17 @@ app.controller('storiesController', function($scope, $http) {
 
 
   })
+
+  $scope.loadMore = function() {
+    var storyIDs = $scope.allStories.slice($scope.fetched+1, $scope.fetched+20)
+    $scope.fetched += 20
+    storyIDs.map(function(story) {
+      $http.get('https://hacker-news.firebaseio.com/v0/item/' + story + '.json?print=pretty').then(function(data) {
+        $scope.stories.push(data.data)
+      })
+
+    })
+  }
 
 })
 
